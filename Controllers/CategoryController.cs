8134d7730d3a411e8f66f5aa2ef73ea977a47235
Joseph Huntley <Joseph.Huntley@outlook.com>
2022-09-23 18:26:53 -0400

    Initial Create

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            // Custom error if the Name is an exact match to DisplayOrder
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                // Change first value to name or display order to have it displayed below name/displayorder
+                ModelState.AddModelError("CustomError", "The Display Order cannot exactly match the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                //Update DB
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Succesfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            // var categoryFromDb = _db.Categories.SingleOrDefault(u=>u.Id==id);
+            // var categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+
+
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            // Custom error if the Name is an exact match to DisplayOrder
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                // Change first value to name or display order to have it displayed below name/displayorder
+                ModelState.AddModelError("CustomError", "The Display Order cannot exactly match the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                //Update DB
+                _db.SaveChanges();
+                TempData["success"] = "Category updated Succesfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            // var categoryFromDb = _db.Categories.SingleOrDefault(u=>u.Id==id);
+            // var categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+
+
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            //Update DB
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Succesfully";
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+// Any services must be added between the line above and var app line below
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("AwsConnection")
+    ));
+
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// Middleware in the pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Authentication
+// Must come before authorization
+// app.UseAuthentication(); 
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
